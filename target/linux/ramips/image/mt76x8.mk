5f53a484f6c249ca72bad5a4fb676a2947ca120d
Harry Kim <harry.kim@newracom.com>
2026-04-16 10:10:40 -0700

    ramips: include wireguard in wemo matter bridge profile

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -69,7 +69,8 @@ define Device/wemo-matter-bridge
   DEVICE_VARIANT := 16M
   DEVICE_PACKAGES := -hostapd-common -iw -kmod-cfg80211 -kmod-mac80211 \
 	-kmod-mt7603 -kmod-mt76x2 -wifi-scripts -wireless-regdb \
-	-wpad-basic-mbedtls libsqlite3 libstdcpp libupnp \
+	-wpad-basic-mbedtls kmod-wireguard libsqlite3 libstdcpp libupnp \
+	wireguard-tools \
 	openwemo-bridge-core wemo-mtd-data wemo-matter-bridge
   SUPPORTED_DEVICES += belkin,wemo-matter-bridge
   SUPPORTED_DEVICES += wemo-matter-bridge
